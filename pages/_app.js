2d4d62ad7162f4b42e4ed1409c3479b6b771537e
Linda Angulo Lopez <angulo.linda@gmail.com>
2022-01-01 21:27:57 +0100

    brough back header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+import Head from 'next/head';
+
+import Layout from '../components/layout/layout';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+      <title>Next.JS Event App</title>
+      <meta 
+      name ='description'
+      content='Events to Build NextJS Developer Skills'
+      />
+        <meta 
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
+  
+
 }
 
 export default MyApp
